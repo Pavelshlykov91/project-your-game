bcbbc7074f3df9fcfa615184b8370c5a509bb2bd
gherage <gerich89@gmail.com>
2023-10-26 17:42:46 +0300

    feat:main

--- client/src/features/main/MainPage.tsx
+++ client/src/features/main/MainPage.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import QuestionItem from './QuestionItem';
+import type { RootState } from '../../redux/store';
+import type { Theme } from './types';
 
-const MainPage = () => {
-  return <div>MainPage</div>;
+const MainPage = (): JSX.Element => {
+  const themes: Theme[] = useSelector((store: RootState): Theme[] => store.main.themes);
+  return (
+    <div className="card_container">
+      {themes.map((theme) => (
+        <div key={theme.id} className="theme_container">
+          <div className="theme_name">{theme.title}</div>
+          <div className="question_container">
+            {theme.question.map((question) => (
+              <QuestionItem key={question.id} question={question} />
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default MainPage;
