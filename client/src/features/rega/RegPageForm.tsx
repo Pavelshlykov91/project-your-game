9452bc8be1154553a0f02fd531e60c890178eb2d
Pavelshlykov91 <pavelshlykov91@gmail.com>
2023-10-27 16:03:28 +0300

    fix all

--- client/src/features/rega/RegPageForm.tsx
+++ client/src/features/rega/RegPageForm.tsx
@@ -17,12 +17,10 @@ function RegPageForm(): JSX.Element {
   const onHandleUserAdd = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
     e.preventDefault();
 
-
     api
       .RegFetch({ login, password })
       .then((data) => {
         console.log(data);
-        
         dispatch({ type: 'users/reg', payload: data });
         navigate('/themes');
       })
@@ -30,17 +28,58 @@ function RegPageForm(): JSX.Element {
   };
 
   return (
-    <div>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        // alignItems: 'center',
+        backgroundImage: 'url("../../../public/fon.jpg")',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        minHeight: '100vh', // чтобы контейнер занимал всю высоту экрана
+      }}
+    >
       <form onSubmit={onHandleUserAdd}>
-        <input value={login} onChange={(e) => setLogin(e.target.value)} name="login" type="text" />
-
+        <input
+          value={login}
+          onChange={(e) => setLogin(e.target.value)}
+          name="login"
+          type="text"
+          style={{
+            padding: '10px',
+            border: '1px solid gray',
+            borderRadius: '5px',
+            marginRight: '10px',
+            marginTop: '20px', // добавляем отступ сверху
+          }}
+        />
         <input
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           name="password"
           type="text"
+          style={{
+            padding: '10px',
+            border: '1px solid gray',
+            borderRadius: '5px',
+            marginRight: '10px',
+            marginTop: '20px', // добавляем отступ сверху
+          }}
         />
-        <button type="submit">Submit</button>
+
+        <button
+          type="submit"
+          style={{
+            padding: '10px',
+            backgroundColor: 'rgb(245, 158, 11)',
+            color: 'white',
+            border: 'none',
+            borderRadius: '5px',
+            marginTop: '20px', // добавляем отступ сверху
+          }}
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
